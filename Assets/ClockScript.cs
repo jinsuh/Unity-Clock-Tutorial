8ff377d59a3ada2560745854ca8b369fed5da4c6
unknown <suhongjin94@gmail.com>
2016-01-07 02:17:25 -0600

    first commit

---
+++ Assets/ClockScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System;
+
+public class ClockScript : MonoBehaviour {
+
+    public GameObject HourHand, MinuteHand, SecondHand;
+    	// Use this for initialization
+    private void RotateByFraction(GameObject obj, float fraction)
+    {
+        Transform transform = obj.GetComponent<Transform>();
+        transform.localRotation = Quaternion.Euler(0, 0, fraction * 360);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        RotateByFraction(HourHand, DateTime.Now.Hour / 12f);
+        RotateByFraction(MinuteHand, DateTime.Now.Minute / 60f);
+        RotateByFraction(SecondHand, DateTime.Now.Second / 60f);
+    }
+}
---
+++ Assets/TimeDisplayScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+public class TimeDisplayScript : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+        string currentTime = DateTime.Now.ToShortTimeString();
+        this.GetComponent<Text>().text = currentTime;
+	}
+}
